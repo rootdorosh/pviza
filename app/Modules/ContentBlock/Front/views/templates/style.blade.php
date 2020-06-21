da795ca7aa1e8113d103c40c4b4a1bb847743f9b
you@example.com <you@example.com>
2020-06-21 22:19:45 +0300

    fix

--- app/Modules/ContentBlock/Front/views/templates/style.blade.php
+++ app/Modules/ContentBlock/Front/views/templates/style.blade.php
@@ -1,3 +1,11 @@
-<article class="post-creative">
-    {!! $block->body !!}
-</article>
+<section class="section section-md">
+    <div class="container">
+        <div class="blog-layout">
+            <div class="blog-layout-main">
+                <article class="post-creative">
+                    {!! $block->body !!}
+                </article>
+            </div>
+        </div>
+    </div>
+<section>
