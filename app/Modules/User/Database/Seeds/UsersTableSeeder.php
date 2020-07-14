222b781ca0a3a3b40acb30592b8c0d5f340058ec
Vasyl Dorosh <vasyldorosh@gmail.com>
2020-07-14 13:15:16 +0300

    mail queue

--- app/Modules/User/Database/Seeds/UsersTableSeeder.php
+++ app/Modules/User/Database/Seeds/UsersTableSeeder.php
@@ -15,35 +15,25 @@ class UsersTableSeeder extends Seeder
     {
         $adminRole = config('roles.models.role')::where('name', '=', 'Admin')->first();
         $permissions = config('roles.models.permission')::all();
-        
+
         $items = [
-            [
-                'email' => 'ls@sunbee.studio',
-                'password' => 'liko101010',
-                'name' => 'LS',
-                'position' => 'Designer',
-            ],
             [
                 'email' => 'vasyldorosh@gmail.com',
                 'password' => 'dorosh123',
                 'name' => 'VD',
                 'position' => 'Backend developer',
             ],
-            [
-                'email' => 'mr.ap2089@gmail.com',
-                'password' => 'ap1021d',
-                'name' => 'AP',
-                'position' => 'Frontend developer',
-            ],
         ];
-        
+
         foreach ($items as $item) {
-            if (config('roles.models.defaultUser')::where('email', '=', $item['email'])->first() === null) {
-                $item['is_active'] = 1;
-                $user = config('roles.models.defaultUser')::create($item);
-                $user->roles()->sync([$adminRole->id]);
+            $user = config('roles.models.defaultUser')::where('email', '=', $item['email'])->first();
+
+            if ($user) {
+                $user->is_active = 1;
+                $user->password = $item['password'];
+                $user->save();
             }
-        }        
+        }
 
     }
 }
