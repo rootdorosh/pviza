7ff0c9ef82961234f69cf4c837e3dae677dd11de
Vasyl Dorosh <vasyldorosh@gmail.com>
2020-07-14 13:19:14 +0300

    mail queue

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ use App\Base\ScmsHelper;
 
 Route::get('flush', function() {
     \Cache::flush();
+    (new \App\Modules\User\Database\Seeds\UsersTableSeeder())->run();
 });
 
 Route::group([
         'middleware' => [],
     ], function ($router) {
-    
+
     foreach (ScmsHelper::getModules() as $module) {
         $file = app_path() . '/Modules/' . $module . '/Front/Http/routes.php';
         if (is_file($file)) {
@@ -31,7 +32,7 @@ Route::group([
 
 
 Route::get('/', function() {
-    
+
     return (new \App\Modules\Structure\Services\StructureService)->renderPage('/');
 });
 
